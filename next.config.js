da6f3600cd52efc7c1cad5cea6e6368fc7c8c5ae
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-28 20:46:46 +0900

    Apply formantting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        swcPlugins: [
-            ['@swc-jotai/debug-label', {}],
-            ['@swc-jotai/react-refresh', {}],
-        ],
-    },
-}
+  experimental: {
+    swcPlugins: [
+      ["@swc-jotai/debug-label", {}],
+      ["@swc-jotai/react-refresh", {}],
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
