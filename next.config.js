0b6510d0a5feaaceef20e3ba653aa77b4fad5a9d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-28 20:44:07 +0900

    Configure jotai SWC plugins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        swcPlugins: [
+            ['@swc-jotai/debug-label', {}],
+            ['@swc-jotai/react-refresh', {}],
+        ],
+    },
+}
 
 module.exports = nextConfig
